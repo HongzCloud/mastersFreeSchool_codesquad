1ebc8d25d2607ea82c4596e4f9db4f50370a8355
HongzCloud <jihong21511@gmail.com>
2020-12-30 16:44:03 +0900

    연습문제1

--- ProgrammingFoundationKnowledge.playground/Contents.swift
+++ ProgrammingFoundationKnowledge.playground/Contents.swift
@@ -24,3 +24,47 @@ func convertToInches(fromMeter: Float) -> Float {
 
 convertToInches(fromMeter: 1.5)
 convertToInches(fromMeter: 0.4)
+
+//MARK:- Exercise questions2
+//Mission1 : Rectangle
+struct Rectangle {
+    let leftTopX: Float = 4.5
+    let leftTopY: Float = 10.5
+    let rightBottomX: Float = 15
+    let rightBottomY: Float = 3.5
+    
+    func printRect() {
+        print("(\(leftTopX), \(rightBottomY)), (\(leftTopX), \(leftTopY)) (\(rightBottomX), \(rightBottomY)), (\(rightBottomX), \(leftTopY))" )
+    }
+}
+
+//Mission2 : 호출하기
+let Rectangle1 = Rectangle()
+Rectangle1.printRect()
+
+//Mission3 : printArea()
+extension Rectangle {
+    func printArea() {
+        let a = rightBottomX-leftTopX
+        let b = leftTopY-rightBottomY
+        let area = (a*b)*(a*b)
+        print(sqrt(Double(area)))
+    }
+}
+
+Rectangle1.printArea()
+
+//Mission4 : Float형
+Rectangle1.printRect()
+
+//Mission5 : center
+
+extension Rectangle {
+    func printCenter() {
+        let x = (rightBottomX+leftTopX)/2
+        let y = (leftTopY+rightBottomY)/2
+        print("(\(x),\(y))")
+    }
+}
+
+Rectangle1.printCenter()
