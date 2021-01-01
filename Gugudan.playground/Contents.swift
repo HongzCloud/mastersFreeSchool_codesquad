623bf0f27d984e927baaf16a00ae45e45f99297c
HongzCloud <jihong21511@gmail.com>
2021-01-01 19:33:12 +0900

    4,5단 입력과 변수

--- Gugudan.playground/Contents.swift
+++ Gugudan.playground/Contents.swift
@@ -7,3 +7,19 @@ for i in 1...9 {
 for i in 1...9 {
     print("3 * \(i) = \(3*i)")
 }
+
+var i = 4 * 1
+
+for x in 1...9 {
+    i = 4*x
+    
+    print("4 * \(x) = \(i)")
+}
+
+i = 5 * 1
+
+for x in 1...9 {
+    i = 5*x
+    
+    print("5 * \(x) = \(i)")
+}
