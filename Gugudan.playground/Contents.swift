98e2d34d18824f9f327747843630f1dda347fc63
HongzCloud <jihong21511@gmail.com>
2021-01-01 19:45:15 +0900

    8,9단 조건문과 2중 반복문

--- Gugudan.playground/Contents.swift
+++ Gugudan.playground/Contents.swift
@@ -1,36 +1,7 @@
 import UIKit
 
-for i in 1...9 {
-    print("2 * \(i) = \(2*i)")
-}
-
-for i in 1...9 {
-    print("3 * \(i) = \(3*i)")
-}
-
-var i = 4 * 1
-
-for x in 1...9 {
-    i = 4*x
-    
-    print("4 * \(x) = \(i)")
-}
-
-i = 5 * 1
-
-for x in 1...9 {
-    i = 5*x
-    
-    print("5 * \(x) = \(i)")
-}
-
-var n = 0
-
-while (n < 9) {
-    n += 1
-    print("6 * \(n) = \(6*n)")
-}
-
-for i in 1...9 {
-    print("7 * \(i) = \(7*i)")
+for j in 1..<10 where j % 2 != 0 {
+    for i in 1..<10 {
+        print("\(j) * \(i) = \(j*i)")
+    }
 }
