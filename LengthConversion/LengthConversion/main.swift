b75c5c797e8ac3b15d98330b116fb43c3b10ac24
HongzCloud <jihong21511@gmail.com>
2021-01-04 00:23:03 +0900

    야드길이 변환

--- LengthConversion/LengthConversion/main.swift
+++ LengthConversion/LengthConversion/main.swift
@@ -57,6 +57,22 @@ func convertToM(inch: String) -> String {
     return out
 }
 
+func convertToYard(cm: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 0.0109361
+    let separatedInput = Float(cm.components(separatedBy: "cm")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "yard"
+    return out
+}
+
+func convertToCM(yard: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 91.44
+    let separatedInput = Float(yard.components(separatedBy: "yard")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "cm"
+    return out
+}
+
 func printValue(_ data: String) {
     print(data)
 }
@@ -71,11 +87,20 @@ func lengthConverter(_ input: String, _ to: String = "" ) {
     else if input.hasSuffix("inch") && to == "" {
         printValue(convertToCM(inch: input))
     }
+    else if input.hasSuffix("yard") && to == "" {
+        printValue(convertToM(cm: convertToCM(yard: input)))
+    }
     else if input.hasSuffix("cm") && to == "inch" {
         printValue(convertToInch(cm: input))
     }
     else if input.hasSuffix("inch") && to == "m" {
         printValue(convertToM(inch: input))
+    }
+    else if input.hasSuffix("yard") && to == "m" {
+        printValue(convertToM(cm: convertToCM(yard: input)))
+    }
+    else if input.hasSuffix("m") && to == "yard" {
+        printValue(convertToYard(cm: convertToCM(m: input)))
     }
     else {
         print("지원하지 않는 단위 입니다.")
@@ -84,11 +109,26 @@ func lengthConverter(_ input: String, _ to: String = "" ) {
 
 //실행
 //사용자가 길이 값을 입력하고 변수에 저장하도록 한다.
-let inputData = readLine() ?? ""
-let separatedInputData = inputData.components(separatedBy: " ")
-if separatedInputData.count == 2 {
-    lengthConverter(separatedInputData[0],separatedInputData[1])
+func startGame() {
+    var gameEnd = false
+    
+    while !gameEnd {
+        let inputData = readLine() ?? ""
+        
+        if inputData == "quit" || inputData == "q" {
+            gameEnd = true
+            return
+        }
+        let separatedInputData = inputData.components(separatedBy: " ")
+        if separatedInputData.count == 2 {
+            lengthConverter(separatedInputData[0],separatedInputData[1])
+        }
+        else {
+            lengthConverter(separatedInputData[0])
+        }
+    
+
+    }
 }
-else {
-    lengthConverter(separatedInputData[0])
-}
+
+startGame()
