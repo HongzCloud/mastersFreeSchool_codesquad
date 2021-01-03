0d22f3670d657866a4d327df3d57f52e2d617801
HongzCloud <jihong21511@gmail.com>
2021-01-03 23:47:16 +0900

    인치길이 변환

--- LengthConversion/LengthConversion/main.swift
+++ LengthConversion/LengthConversion/main.swift
@@ -7,21 +7,6 @@
 
 import Foundation
 
-func convertToM(cm: Int) {
-    print("\(Float(cm) / 100) m")
-}
-
-func convertToCM(m: Float) {
-    print("\(Int(m * 100)) cm")
-}
-
-convertToM(cm: 120)
-convertToCM(m: 1.86)
-
-//함수
-//센티미터 단위 값을 변수에 저장하고 변환하는데 사용한다.
-var data: String = "183cm"
-
 //센티미터 단위 값을 저장한 변수를 미터 단위 값으로 변환한 후 변수에 저장하고 저정한 변수 값을 출력한다.
 func convertToM(cm: String) -> String {
    
@@ -35,8 +20,40 @@ func convertToM(cm: String) -> String {
 func convertToCM(m: String) -> String {
     //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
     let convertFormulaNumber: Float = 100
-    let separatedInput = Float(m.components(separatedBy: "cm")[0])! * convertFormulaNumber
+    let separatedInput = Float(m.components(separatedBy: "m")[0])! * convertFormulaNumber
     let out: String = String(separatedInput) + "cm"
+    return out
+}
+
+func convertToCM(inch: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 2.54
+    let separatedInput = Float(inch.components(separatedBy: "inch")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "cm"
+    return out
+}
+
+func convertToInch(cm: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 0.39
+    let separatedInput = Float(cm.components(separatedBy: "cm")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "Inch"
+    return out
+}
+
+func convertToInch(m: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 39
+    let separatedInput = Float(m.components(separatedBy: "m")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "Inch"
+    return out
+}
+
+func convertToM(inch: String) -> String {
+    //길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+    let convertFormulaNumber: Float = 0.0254
+    let separatedInput = Float(inch.components(separatedBy: "inch")[0])! * convertFormulaNumber
+    let out: String = String(separatedInput) + "m"
     return out
 }
 
@@ -44,16 +61,34 @@ func printValue(_ data: String) {
     print(data)
 }
 //문자열로 값 뒤에 붙어있는 단위에 따라 길이를 변환해서 결과를 출력하는 함수를 만든다.
-func lengthConverter(_ input: String) {
-    if input.hasSuffix("cm") {
+func lengthConverter(_ input: String, _ to: String = "" ) {
+    if input.hasSuffix("cm") && to == "" {
         printValue(convertToM(cm: input))
     }
-    else {
+    else if input.hasSuffix("m") && to == "" {
         printValue(convertToCM(m: input))
+    }
+    else if input.hasSuffix("inch") && to == "" {
+        printValue(convertToCM(inch: input))
+    }
+    else if input.hasSuffix("cm") && to == "inch" {
+        printValue(convertToInch(cm: input))
+    }
+    else if input.hasSuffix("inch") && to == "m" {
+        printValue(convertToM(inch: input))
+    }
+    else {
+        print("지원하지 않는 단위 입니다.")
     }
 }
 
-lengthConverter(data)
+//실행
 //사용자가 길이 값을 입력하고 변수에 저장하도록 한다.
-let inputData = readLine()
-lengthConverter(inputData ?? "")
+let inputData = readLine() ?? ""
+let separatedInputData = inputData.components(separatedBy: " ")
+if separatedInputData.count == 2 {
+    lengthConverter(separatedInputData[0],separatedInputData[1])
+}
+else {
+    lengthConverter(separatedInputData[0])
+}
